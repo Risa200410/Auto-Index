843eec9029b12fe590333fc077b93b067e7a1eb6
Maritza Ulfa Huriyah <maritza.ulfa.huriyah@gmail.com>
2026-06-12 18:14:23 +0700

    coba 2

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ import pandas as pd
 import streamlit as st
 
 st.set_page_config(
-    page_title="AutoIndex",
-    page_icon="📑📑📑",
+    page_title="AutoIndexxx",
+    page_icon="📑",
     layout="centered",
     initial_sidebar_state="expanded", # Expanded because we have sidebar now
 )
