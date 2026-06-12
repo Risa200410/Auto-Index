42eda2f0185c5747ec636dae492506f70933989b
Maritza Ulfa Huriyah <maritza.ulfa.huriyah@gmail.com>
2026-06-12 18:13:12 +0700

    Coba hosting

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import streamlit as st
 
 st.set_page_config(
     page_title="AutoIndex",
-    page_icon="📑",
+    page_icon="📑📑📑",
     layout="centered",
     initial_sidebar_state="expanded", # Expanded because we have sidebar now
 )
